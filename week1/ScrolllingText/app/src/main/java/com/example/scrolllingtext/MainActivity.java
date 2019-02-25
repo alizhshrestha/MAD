0f51e26c6de274483fc3625eef293d2c2f1a4680
Alish Shrestha <alizhshrestha@gmail.com>
2019-02-25 09:06:14 +0545

    Scrolling Text 2 assessment completed

---
+++ week1/ScrolllingText/app/src/main/java/com/example/scrolllingtext/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.scrolllingtext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
